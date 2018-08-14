fba8a7d38bab5bb36951aa7ee73602b7f196b8c4
Serghei Mangul <serghei@login4.(none)>
2018-08-13 18:56:58 -0700

    change instalation - add conda

--- install.sh
+++ install.sh
@@ -1,1 +1,15 @@
-pip install pysam
+cd tools
+./install-MiniConda.sh
+cd MiniConda/lib
+ln -s libncursesw.so.5 libtinfow.so.5
+
+
+
+cd ..
+cd bin
+./conda install -c bioconda pysam
+./conda install -c anaconda bzip2 
+
+
+MiniConda="$PWD/MiniConda/bin/python"
+
--- umi.reducer.sh
+++ umi.reducer.sh
@@ -7,6 +7,15 @@ parser.add_argument('bam',help='bam with mapped and unmapped reads, BAM file nee
 #parser.add_argument('-chr', '--chr', default='', type=str,help='Use this option to specify the format of chromosome name in the bam file. By default it is : 1,2,3,...,X. In case the format is : chr1, chr2,..,chrX, use : -chr chr)')
 EOF
 
+
+DIR_CODE=`dirname $(readlink -f "$0")`
+
+if [ -d "$DIR_CODE/tools/MiniConda/bin" ]; then
+    echo "Add MiniConda to PATH if it's available"
+    export PATH="$DIR_CODE/tools/MiniConda/bin:$PATH"
+fi
+
+
 #filename=$(basename "$BAM")
 extension="${BAM##*.}"
 filename="${BAM%.*}"
@@ -21,12 +30,13 @@ echo "Start analysis"
 
 #collapsePCRduplicates.py
 python ${dirSource}/collapsePCRduplicates.py  ${BAM} ${filename}_PCRduplicates_removed.bam
-${dirSource}/tools/samtools-1.3/samtools sort ${filename}_PCRduplicates_removed.bam >${filename}_PCRduplicates_removed.sort.bam
+samtools sort ${filename}_PCRduplicates_removed.bam >${filename}_PCRduplicates_removed.sort.bam
 
 rm ${filename}_PCRduplicates_removed.bam
-${dirSource}/tools/samtools-1.3/samtools index ${filename}_PCRduplicates_removed.sort.bam
+samtools index ${filename}_PCRduplicates_removed.sort.bam
 
 echo "Run genomic.categories.py ..."
+
 
 python ${dirSource}/mRNA.abundance.py --perCategory --mouse --multi ${filename}_PCRduplicates_removed.sort.bam ${filename}
 
